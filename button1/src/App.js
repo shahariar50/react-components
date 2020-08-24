8a709fd59dd103f5edc7506753b43dabbe0df158
Shahariar Touhid <shahariar.touhid@gmail.com>
2020-08-24 16:42:34 +0600

    Added disable and icon feature to button1

--- button1/src/App.js
+++ button1/src/App.js
@@ -8,76 +8,121 @@ function App() {
       <div>
         <h1>Teal</h1>
         <Button variantColor="teal" size="xs" variant="solid">
-          Button
+          Solid
         </Button>
         <Button variantColor="teal" size="sm" variant="outline">
-          Button
+          Outline
         </Button>
         <Button variantColor="teal" size="md" variant="link">
-          Button
+          Link
         </Button>
         <Button variantColor="teal" size="lg" variant="ghost">
-          Button
+          Ghost
+        </Button>
+        <Button variantColor="teal" size="md" leftIcon="email">
+          Email
+        </Button>
+        <Button variantColor="teal" size="md" rightIcon="arrow-forward">
+          Call Us
+        </Button>
+        <Button variantColor="teal" size="md" disabled>
+          Disabled
         </Button>
       </div>
       <div>
         <h1>Green</h1>
         <Button variantColor="green" size="xs" variant="solid">
-          Button
+          Solid
         </Button>
         <Button variantColor="green" size="sm" variant="outline">
-          Button
+          Outline
         </Button>
         <Button variantColor="green" size="md" variant="link">
-          Button
+          Link
         </Button>
         <Button variantColor="green" size="lg" variant="ghost">
-          Button
+          Ghost
+        </Button>
+        <Button variantColor="green" size="md" leftIcon="email">
+          Email
+        </Button>
+        <Button variantColor="green" size="md" rightIcon="arrow-forward">
+          Call Us
+        </Button>
+        <Button variantColor="green" size="md" disabled>
+          Disabled
         </Button>
       </div>
       <div>
         <h1>Red</h1>
         <Button variantColor="red" size="xs" variant="solid">
-          Button
+          Solid
         </Button>
         <Button variantColor="red" size="sm" variant="outline">
-          Button
+          Outline
         </Button>
         <Button variantColor="red" size="md" variant="link">
-          Button
+          Link
         </Button>
         <Button variantColor="red" size="lg" variant="ghost">
-          Button
+          Ghost
+        </Button>
+        <Button variantColor="red" size="md" leftIcon="email">
+          Email
+        </Button>
+        <Button variantColor="red" size="md" rightIcon="arrow-forward">
+          Call Us
+        </Button>
+        <Button variantColor="red" size="md" disabled>
+          Disabled
         </Button>
       </div>
       <div>
         <h1>Blue</h1>
         <Button variantColor="blue" size="xs" variant="solid">
-          Button
+          Solid
         </Button>
         <Button variantColor="blue" size="sm" variant="outline">
-          Button
+          Outline
         </Button>
         <Button variantColor="blue" size="md" variant="link">
-          Button
+          Link
         </Button>
         <Button variantColor="blue" size="lg" variant="ghost">
-          Button
+          Ghost
+        </Button>
+        <Button variantColor="blue" size="md" leftIcon="email">
+          Email
+        </Button>
+        <Button variantColor="blue" size="md" rightIcon="arrow-forward">
+          Call Us
+        </Button>
+        <Button variantColor="blue" size="md" disabled>
+          Disabled
         </Button>
       </div>
       <div>
         <h1>Yellow</h1>
         <Button variantColor="yellow" size="xs" variant="solid">
-          Button
+          Solid
         </Button>
         <Button variantColor="yellow" size="sm" variant="outline">
-          Button
+          Outline
         </Button>
         <Button variantColor="yellow" size="md" variant="link">
-          Button
+          Link
         </Button>
         <Button variantColor="yellow" size="lg" variant="ghost">
-          Button
+          Ghost
+        </Button>
+        <Button variantColor="yellow" size="md" leftIcon="email">
+          Email
+        </Button>
+        <Button variantColor="yellow" size="md" rightIcon="arrow-forward">
+          Call Us
+        </Button>
+        <Button variantColor="yellow" size="md" disabled>
+          Disabled
         </Button>
       </div>
     </div>
--- button1/src/components/Button.js
+++ button1/src/components/Button.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./button.css";
+import { Icon } from "@chakra-ui/core";
 
-const Button = ({ children, ...props }) => {
+const Button = (props) => {
   const variantColor =
     props.variantColor && props.variantColor !== true
       ? "variantColor-" + props.variantColor
@@ -12,10 +13,38 @@ const Button = ({ children, ...props }) => {
     props.variant && props.variant !== true
       ? "variant-" + props.variant
       : "variant-solid";
+  const disabled = props.disabled ? "disabled" : "";
+
+  const children = () => {
+    if (props.leftIcon) {
+      return (
+        props.leftIcon !== true && (
+          <>
+            <Icon name={props.leftIcon} className="leftIcon" />
+            {props.children}
+          </>
+        )
+      );
+    } else if (props.rightIcon) {
+      return (
+        props.rightIcon !== true && (
+          <>
+            {props.children}
+            <Icon name={props.rightIcon} className="rightIcon" />
+          </>
+        )
+      );
+    }
+
+    return props.children;
+  };
 
   return (
-    <button className={`btn ${variantColor} ${size} ${variant}`}>
-      {children}
+    <button
+      className={`btn ${variantColor} ${size} ${variant} ${disabled}`}
+      disabled={props.disabled ? true : false}
+    >
+      {children()}
     </button>
   );
 };
